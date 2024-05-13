9ac9208bb0146dceab1213addde131024e233ccb
YoungManPeach <jgmmarks@gmail.com>
2024-05-13 13:26:58 +0100

    Add All Files

---
+++ main.js
@@ -0,0 +1,114 @@
+const playersAtTable = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+const randomPlayersIndex = Math.floor(Math.random() * playersAtTable.length);
+const randomPlayers = playersAtTable[randomPlayersIndex];
+
+const betTypes = ["1num", "2num", "4num", "5num", "6num", "12num", "18num"];
+const randomBetTypeIndex = Math.floor(Math.random() * betTypes.length);
+const randomBetType = betTypes[randomBetTypeIndex];
+
+const betOdds = {
+    "1num": 0.027,
+    "2num": 0.054,
+    "3num": 0.081,
+    "4num": 0.1081,
+    "5num": 0.1351,
+    "6num": 0.1621,
+    "12num": 0.3243,
+    "18num": 0.4865,
+}
+
+function mainLoop() {
+    if (randomPlayers == 2) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+    } else if (randomPlayers == 3) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+    } else if (randomPlayers == 4) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+    } else if (randomPlayers == 5) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+    } else if (randomPlayers == 6) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+        player6Result = placeYourBet();
+    } else if (randomPlayers == 7) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+        player6Result = placeYourBet();
+        player7Result = placeYourBet();
+    } else if (randomPlayers == 8) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+        player6Result = placeYourBet();
+        player7Result = placeYourBet();
+        player8Result = placeYourBet();
+    } else if (randomPlayers == 9) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+        player6Result = placeYourBet();
+        player7Result = placeYourBet();
+        player8Result = placeYourBet();
+        player9Result = placeYourBet();
+    } else if (randomPlayers == 10) {
+        player1Result = placeYourBet();
+        player2Result = placeYourBet();
+        player3Result = placeYourBet();
+        player4Result = placeYourBet();
+        player5Result = placeYourBet();
+        player6Result = placeYourBet();
+        player7Result = placeYourBet();
+        player8Result = placeYourBet();
+        player9Result = placeYourBet();
+        player10Result = placeYourBet();
+    }
+}
+
+
+
+function placeYourBet() {
+    let toBeatRaw = Math.random();
+    let toBeatRounded = toBeatRaw.toFixed(4);
+    let betWins = false;
+    let betType = randomBetType;
+    const ourBet = betOdds[betType];
+    
+   // console.log("DEBUG INFO:");
+   // console.log("To Beat:", toBeatRounded);
+   // console.log("Our bet: ", ourBet);
+   // console.log("bet type: ", betType);
+
+    if (ourBet > toBeatRounded) {
+        betWins = true;
+    } else {
+        betWins = false;
+    }
+
+    console.log("Bet Wins?", betWins);
+
+    return betWins;
+
+    
+}
+
+mainLoop()
